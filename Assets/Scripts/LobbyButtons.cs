86800d07dc74c168639ff81e4275131eb71c09af
EXHOUL <wikishek@hotmail.com>
2020-11-23 20:53:24 +0100

    LaParteDeAntonioDeLobby
    Escena de Lobby y sala de espera añadidas.

---
+++ Assets/Scripts/LobbyButtons.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.Networking.Match;
+
+public class LobbyButtons : MonoBehaviour
+{
+    private NetworkManager networkManager;
+
+    void Start()
+    {
+        networkManager = NetworkManager.singleton;
+    }
+
+    public void LeaveRoom()
+    {
+        MatchInfo matchInfo = networkManager.matchInfo;
+        networkManager.matchMaker.DropConnection(matchInfo.networkId, matchInfo.nodeId, 0, networkManager.OnDropConnection);
+        networkManager.StopHost();
+    }
+
+    public void StartGame()
+    {
+        NetworkManager.singleton.ServerChangeScene("Blocking");
+    }
+
+    public void QuitGame()
+    {
+        NetworkManager.singleton.ServerChangeScene("Blocking");
+        LeaveRoom();
+    }
+}
